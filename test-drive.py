1f165e40cea91ec49368aa4897e793077dcbfca1
Asadbek <asadbekn443@gmail.com>
2025-09-05 16:46:13 -0400

    forward

--- test-drive.py
+++ test-drive.py
@@ -10,10 +10,10 @@ bot = HamBot(lidar_enabled=False, camera_enabled=False)
 print("Starting forward drive for 2 seconds...")
 
 # Convention: left motor = negative speed, right motor = positive speed
-bot.set_left_motor_speed(-50)   # left reversed
+bot.set_left_motor_speed(50)   # left reversed
 bot.set_right_motor_speed(50)   # right forward
 
-time.sleep(2)  # drive forward for 2 seconds
+time.sleep(6)  # drive forward for 2 seconds
 
 print("Stopping motors.")
 bot.stop_motors()
